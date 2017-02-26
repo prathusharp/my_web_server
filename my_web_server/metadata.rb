9647b3aaed5d0e1fecaf4f9bf9c9abd18fa22b6d
root <root@prathusha.novalocal>
2017-02-26 20:38:35 +0000

    initial commit of my_web_server cookbook

---
+++ my_web_server/metadata.rb
@@ -0,0 +1,19 @@
+name 'my_web_server'
+maintainer 'The Authors'
+maintainer_email 'you@example.com'
+license 'all_rights'
+description 'Installs/Configures my_web_server'
+long_description 'Installs/Configures my_web_server'
+version '0.1.0'
+
+# The `issues_url` points to the location where issues for this cookbook are
+# tracked.  A `View Issues` link will be displayed on this cookbook's page when
+# uploaded to a Supermarket.
+#
+# issues_url 'https://github.com/<insert_org_here>/my_web_server/issues' if respond_to?(:issues_url)
+
+# The `source_url` points to the development reposiory for this cookbook.  A
+# `View Source` link will be displayed on this cookbook's page when uploaded to
+# a Supermarket.
+#
+# source_url 'https://github.com/<insert_org_here>/my_web_server' if respond_to?(:source_url)
